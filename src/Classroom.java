2c502949a444dca03fe8bc0fb7316142ffeb1417
Your Name <dat054760@gmail.com>
2022-09-29 09:12:56 +0700

    class 1

--- src/Classroom.java
+++
@@ -1,3 +0,0 @@
-public class Classroom {
-    int id;
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world cc !");
-        System.out.println("adu");
-
-    }
-}
